9c01d4738a4a010e0b38ba01861881793fa7d9a0
ismail debbarh <usamdh07@gmail.com>
2024-01-03 14:52:19 +0100

    Add search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,30 @@
 'use client';
-
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const [query, setQuery] = useState(
+    searchParams.get('query')?.toString() || '',
+  );
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    inputChangeHandler();
+  }, [query]);
+
+  function inputChangeHandler() {
+    const params = new URLSearchParams(searchParams);
+    if (query) {
+      params.set('query', query);
+    } else {
+      params.delete('query');
+    }
+    router.replace(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +33,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
